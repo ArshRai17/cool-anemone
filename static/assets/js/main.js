c1c4923a330a52473999ca9ed16c01db420b7f15
Stackbit Code Editor <projects@stackbit.com>
2021-08-29 01:56:12 +0000

    main.js: changed

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -16,7 +16,7 @@ if (menuToggle) {
     document.body.classList.toggle('menu--opened');
     e.preventDefault();
   },false);
-  document.body.classList.remove('');
+  document.body.classList.remove('menu--opened');
 
   window.addEventListener('resize', function () {
     if (menuToggle.offsetParent === null) {
